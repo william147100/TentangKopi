51643d14819712b8628ec31dbab835a8b8348de4
ricky hansen kurnia <ricky.hansen123@users.noreply.github.com>
2020-12-10 12:13:47 +0700

    baru js submit

--- Homepage/scripthome.js
+++ Homepage/scripthome.js
@@ -27,3 +27,75 @@ function showSlides() {
 $("#Top").click(function(){
   scroll(0,0);
  });
+
+ 
+ function profileValidate()
+ {
+   var nama1= document.getElementById("name1Txt").value;
+   var nama2= document.getElementById("name2Txt").value;
+   var address = document.getElementById("addressTxt").value;
+   var email = document.getElementById("emailTxt").value;
+   var url = document.getElementById("urlTxt").value;
+   var bio = document.getElementById("bioTxt").value;
+
+   
+   if(nama1 =="")
+   {
+     alert("nama harus di isi");
+     
+   }else if(nama1.length <3)
+   {
+     alert("Nama Minimal 3 karakter");
+   }
+   else if(nama1.length >20)
+   {
+     alert("Karakter yang digunakan terlalu panjang");
+   }
+   else if(nama2 =="")
+   {
+     alert("Username harus di isi");
+     
+   }else if(nama2.length <3)
+   {
+     alert("Username Minimal 3 karakter");
+   }
+   else if(nama2.length >20)
+   {
+     alert("Karakter yang digunakan terlalu panjang");
+   }
+   else if(email=="")
+   {
+     alert("masukan alamat email");
+   }
+   else if(address=="")
+   {
+     alert("address harus di isi");
+   }
+   else if(url=="")
+   {
+     alert("Link Url harus di isi");
+   }
+   else if(bio=="")
+   {
+     alert("biodata harus di isi");
+   }
+   else{
+    alert("Profile berhasil di Update !");
+   }
+   
+
+   function check_email(){
+    var email = document.getElementById("emailTxt").value;
+    if(!email.match(/\S+@\S+\.\S+/)){ // Jaymon's / Squirtle's solution
+        // Do something
+        alert("Masukan Alamat email yang valid");
+        return false;
+    }
+    if(email.indexOf(' ')!=-1 || val.indexOf('..')!=-1){
+      alert("Masukan Alamat email yang valid");
+        // Do something
+        return false;
+    }
+    return true;
+}
+ }
